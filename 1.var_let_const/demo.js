d5a9e61368f391bcf476e09b48527ee6e77e4216
prasadt865 <talebailkarprasad865@gmail.com>
2023-11-23 12:31:54 +0000

    simple update

---
+++ 1.var_let_const/demo.js
@@ -0,0 +1,9 @@
+var dummy="not used";
+const accountId=65;
+let accountEmail="pt@gmail.com";
+dommy="undefined variable"
+
+let check;
+console.table([dummy,accountId,accountEmail,dommy,check]);
+
+
---
+++ 2.datatypes/demo.js
@@ -0,0 +1,11 @@
+"use strict";//used to apply modern javascript
+
+let age=30;
+
+let name="prasad";
+
+let job=false;
+
+console.table([age,name,job]);
+console.log(typeof null);
+console.log(typeof undefined);
