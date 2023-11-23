1d95e4c26ac89c0c483ffe6d67c111bfcbe16f00
prasadt865 <talebailkarprasad865@gmail.com>
2023-11-23 13:40:50 +0000

    edited

---
+++ 1.Basics/03_conversion.js
@@ -0,0 +1,30 @@
+let score="33";
+let scoreAns="33abc";
+let var1=null;
+let var2=undefined;
+let var3=true;
+let var4=false;
+let var5="prasad";
+
+console.log(Number(score))
+console.log(Number(scoreAns))
+console.log(Number(var1))
+console.log(Number(var2))
+console.log(Number(var3))
+console.log(Number(var4))
+console.log(Number(var5))
+console.log(typeof NaN);
+
+console.log(String(33))
+
+console.log(String(true))
+
+console.log(String(undefined))
+
+console.log(String(null))
+
+console.log(String(false))
+
+console.log(Boolean(""))
+console.log(Boolean("prasad"))
+console.log(Boolean(1))
