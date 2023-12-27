202e6328757461217648363cf4a814425f030251
prasadt865 <talebailkarprasad865@gmail.com>
2023-12-27 08:08:41 +0000

    numbers and math

---
+++ 1.Basics/09_number.js
@@ -0,0 +1,11 @@
+const num=10
+
+let value = new Number(1121.6721)
+console.log(value.toString())
+console.log(value.valueOf())
+console.log(value.toFixed(2))
+console.log(value.toPrecision(3))
+value=100000
+console.log(value.toLocaleString('en-IN'))
+console.log(value.toLocaleString())
+
---
+++ 1.Basics/10_maths.js
@@ -0,0 +1,29 @@
+const num=10
+
+let value = new Number(10)
+
+console.log(Math.PI);
+console.log(Math.abs(-5));
+console.log(Math.pow(2,3));
+console.log(Math.floor(2.2));
+console.log(Math.ceil(2.2));
+
+let arr=new Array(1,2,3);
+console.log(arr);
+console.log(Math.max(...arr));
+console.log(Math.min(...arr));
+
+console.log(Math.floor(Math.random()*10));
+
+let min=10
+let max=100
+let range=max-min
+console.log(Math.floor(Math.random()*range +min));
+
+for(i=0;i<100;i++)
+{
+    if((Math.floor(Math.random()*range +min)==99))
+        console.log("99 generated")
+}
+
+
